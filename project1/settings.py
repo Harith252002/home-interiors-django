85b6dd16d5705b8b50765eef48338ba16fa7b0ef
Harith252002 <palakaveetiharitha25@gmail.com>
2025-04-02 22:14:24 +0530

    Update settings.py

--- project1/settings.py
+++ project1/settings.py
@@ -16,10 +16,12 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+
+ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 
 # Quick-start development settings - unsuitable for production
