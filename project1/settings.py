9bcc02760bfb65e6c12df970dc9ede64337949bc
Harith252002 <palakaveetiharitha25@gmail.com>
2025-04-02 22:31:56 +0530

    Update settings.py

--- project1/settings.py
+++ project1/settings.py
@@ -17,10 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-
-
- STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 
